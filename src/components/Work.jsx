5346f65d79ce6c10add9b126bb22d86f27d9a27f
Rajesha T <rajesha@wittypen.com>
2022-10-10 20:20:24 +0530

    :lipstick: Add Food App

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Todo from "../assets/todo.jpg";
 import Notes from "../assets/notes.jpg";
 import MemeGenerator from "../assets/memegenerator.jpg";
+import FoodDelivery from "../assets/fooddelivery.jpeg";
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-[#ccd6f6] bg-[#023a66]">
@@ -99,6 +100,38 @@ const Work = () => {
                 </a>
                 <a
                   href="https://github.com/RajeshxD/todo-app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white to-gray-700 font-black text-lg text-gray-800">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid-item */}
+          <div
+            style={{ backgroundImage: `url(${FoodDelivery})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Food Delivery
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://restaurant-rajesha.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white to-gray-700 font-black text-lg text-gray-800">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/RajeshxD/restaurantapp"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
